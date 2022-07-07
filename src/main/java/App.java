3a295e35ca6199c823f3fa74b3618e4d48211800
kimdonghyeon <sh2115521@naver.com>
2022-07-07 14:32:24 +0900

    명령 프롬프트 생성

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class App {
 
     public void run(){
-        System.out.println("ssg 실행");
+        System.out.println("===== 명언 SSG ====");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        outer : while(true){
+            System.out.print(" 명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch( cmd ){
+                case "종료":
+                    break outer;
+                case "":
+            }
+        }
     }
 }
