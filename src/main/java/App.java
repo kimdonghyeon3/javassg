1878d7608416a49fb2e6c01e1b220fc395c764ee
kimdonghyeon <sh2115521@naver.com>
2022-07-07 15:46:03 +0900

    글 등록시 마다 글 번호 증가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class App {
 
-    public void run(){
+    public void run() throws IOException {
         System.out.println("===== 명언 SSG ====");
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = 0;
 
         outer : while(true){
             System.out.print(" 명령) ");
-            String cmd = sc.nextLine().trim();
+            String cmd = br.readLine().trim();
 
-            switch( cmd ){
+            switch( cmd ) {
                 case "종료":
                     break outer;
-                case "":
+                case "등록":
+                    String saying;
+                    String author;
+                    System.out.print(" 명언 : ");
+                    saying = br.readLine().trim();
+                    System.out.print(" 작가 : ");
+                    author = br.readLine().trim();
+                    System.out.println(num++ +"번 명언이 저장되었습니다.");
+
+
+                    break;
+
+                case "목록":
+
+
+                case "삭제":
+                case "수정":
+
             }
         }
     }
+
+    public void json(int num, String author, String saying){
+
+    }
+
+
+
 }
